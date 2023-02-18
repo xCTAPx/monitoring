709f553e9561ad73491270ee08b264e6fa48053f
oleg <egormaysnikov@gmail.com>
2023-02-18 14:36:26 +0300

    add ceil info

---
+++ client/src/UI/CeilInfo.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+
+type TDatas = {
+    title: string,
+    data: TData[]
+}
+type TData = {
+    label: string,
+    value: number
+}
+type Props = { data: TDatas }
+const Title = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    border: 2px solid #8D9595 ;
+`
+const Ceil = styled.div`
+    width: 150px;
+    height: min-content;
+    padding: 10px;
+    border-radius: 10px;
+    overflow: hidden;
+    background-color: #414F4F;
+    color: white;
+    font-weight: bold;
+    margin: 30px;
+`
+const ParList = styled.ul`
+    list-style: none;
+    padding: 11px;
+    margin: 0;
+`
+const CeilLi = styled.li<{ statusColor: string }>`
+    display: flex;
+    justify-content: space-between;
+    background-color: #414F4F;
+    border-radius: 5px;
+    margin-top: 3px;
+    margin-bottom: 3px;
+    padding: 5px;
+    ${({ statusColor }) => statusColor == 'danger' ?
+        "background-color: #EB5835;"
+        :
+        ""};
+    ${({ statusColor }) => statusColor == 'warning' ?
+        "background-color: #FAB82E;"
+        :
+        ""};
+`
+
+export const CeilInfo: React.FC<Props> = ({ data }) => {
+    return (
+        <Ceil>
+            <Title>{data.title}</Title>
+            <ParList>
+                {data.data.map(e => {
+                    return (
+                        <CeilLi><span>{e.label}</span><span>{e.value}</span></CeilLi>
+                    )
+                })}
+            </ParList>
+        </Ceil>
+    )
+}
--- client/src/pages/Details/Details.tsx
+++ client/src/pages/Details/Details.tsx
@@ -1,7 +1,145 @@
 import React from 'react'
 import { useLocation, useParams } from 'react-router-dom'
+import styled from 'styled-components'
+import { MainLayout } from '../../layouts'
+import { CeilInfo } from '../../UI/CeilInfo'
 
 type Props = {}
+
+const Grid = styled.div`
+    display: flex;
+`
+
+const infoData = [
+    {
+        title: 'ПС - 1',
+        data: [
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+        ]
+    },
+    {
+        title: 'ПС - 1',
+        data: [
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+        ]
+    },
+    {
+        title: 'ПС - 1',
+        data: [
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+        ]
+    },
+    {
+        title: 'ПС - 1',
+        data: [
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+        ]
+    },
+    {
+        title: 'ПС - 1',
+        data: [
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            },
+            {
+                label: "T C",
+                value: 220
+            }, {
+                label: "T C",
+                value: 220
+            },
+        ]
+    }
+]
 
 export const Details: React.FC<Props> = () => {
     // Below are examples for extracting params from path and state
@@ -10,5 +148,9 @@ export const Details: React.FC<Props> = () => {
     console.log(location)
     console.log(params)
 
-    return <h1>Details</h1>
+    return <MainLayout screenTitle='Эксгаустер' title='Эксгаустер номер один'>
+        <Grid>
+            {infoData.map(e => <CeilInfo data={e} />)}
+        </Grid>
+    </MainLayout>
 }
